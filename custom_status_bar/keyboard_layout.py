b4d48dc995021b5c7d1b026e1e6a9445f83762a0
thorgeir93 <sigurdson22@gmail.com>
2016-09-21 20:48:10 +0000

    Init

---
+++ custom_status_bar/keyboard_layout.py
@@ -0,0 +1,61 @@
+from i3pystatus import IntervalModule
+import subprocess
+
+
+class Xkblayout(IntervalModule):
+    """Displays and changes current keyboard layout.
+
+    ``change_layout`` callback finds the current layout in the
+    ``layouts`` setting and enables the layout following it. If the
+    current layout is not in the ``layouts`` setting the first layout
+    is enabled.
+
+    ``layouts`` can be stated with or without variants,
+    e.g.: status.register("xkblayout", layouts=["de neo", "de"])
+    """
+
+    interval = 1
+    format = u"{name}"
+    uppercase = True
+    settings = (
+        ("format", "Format string"),
+        ("layouts", "List of layouts"),
+        ("uppercase", "Flag for uppercase output"),
+    
+        )
+    layouts = []
+    on_leftclick = "change_layout"
+
+    def run(self):
+        kblayout = self.kblayout()
+
+        full_text = self.format.format(name=kblayout)
+        if self.uppercase:
+            full_text = full_text.upper()
+
+        self.output = {
+            "full_text": full_text,
+            "color": "#ffffff"
+        
+        }
+
+    def change_layout(self):
+        layouts = self.layouts
+        kblayout = self.kblayout()
+        if kblayout in layouts:
+            position = layouts.index(kblayout)
+            try:
+                subprocess.check_call(["setxkbmap"] +
+                                      layouts[position + 1].split())
+            except IndexError:
+                subprocess.check_call(["setxkbmap"] + layouts[0].split())
+        else:
+            subprocess.check_call(["setxkbmap"] + layouts[0].split())
+
+    def kblayout(self):
+        kblayout = subprocess.check_output("setxkbmap -query", shell=True)\
+            .decode("utf-8").splitlines()
+        kblayout = [l.split() for l in kblayout]
+        kblayout = [l[1].strip() for l in kblayout
+                    if l[0].startswith(("layout", "variant"))]
+        return (" ").join(kblayout)
---
+++ status_bar.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from i3pystatus import Status
+from custom_status_bar.keyboard_layout import Xkblayout
+
+status = Status()
+
+status.register(Xkblayout)
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+        format="%a %-d %b %H:%M:%S",)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
+
+# Displays whether a DHCP client is running
+status.register("runwatch",
+    name="DHCP",
+    path="/var/run/dhclient*.pid",)
+
+# Note: the network module requires PyPI package netifaces
+status.register("network",
+    interface="wlp2s0",
+    format_up="wifi: {v4cidr} {kbs} MB/s ",
+    divisor=1048576,
+    round_size=2,
+    )
+
+status.register("network",
+    interface="tun0",
+    format_up="tun0: {v4cidr} {kbs} MB/s ",
+    divisor=1048576,
+    round_size=2,
+    )
+
+status.register("network",
+    interface="virbr0",
+    format_up="virbr0: {v4cidr} {kbs} MB/s ",
+    divisor=1048576,
+    round_size=2,
+    )
+
+
+
+#status.register(
+#    'battery',
+#    interval=5,
+#    format='{battery_ident}: [{status} ]{percentage_design:.2f}%',
+#    alert=True,
+#    alert_percentage=15,
+#    status = {
+#        'DPL': 'DPL',
+#        'CHR': 'CHR',
+#        'DIS': 'DIS',
+#        'FULL': '',})
+
+#status.register(
+#    "backlight"
+#        )
+
+
+#status.register("network",
+#    interface="corpnet",
+#    format_up="corpnet: {v4cidr}",)
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+#status.register("disk",
+#    path="/home/throstur",
+#    format="/home/throstur {avail}",)
+#status.register("disk",
+#    path="/",
+#    format="/ {avail}",)
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+    format="♪{volume}",)
+
+
+status.run()
