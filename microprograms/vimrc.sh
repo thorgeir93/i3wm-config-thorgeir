68959ef511b8b47050934cd93c1f28c1eccf4aa6
thorgeir sigurdsson <thorgeirsigurd@gmail.com>
2017-02-14 19:22:51 +0000

    Shorcut to add a basic .vimrc config to a file.

---
+++ microprograms/vimrc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Execute a basic vimrc configs for a given vim file.
+
+# :set ts=4
+# :set exapandtab
+# :retab
+# :set bg
+
+# Just to be sure the commands will not 
+# be execute alongside mines.
+sleep 1
+xdotool key colon s e t space t s equal 4 Return 
+xdotool key colon s e t space e x p a n d t a b Return 
+xdotool key colon r e t a b Return 
+xdotool key colon s e t space b g equal d a r k Return
