f66a75b3402dc13dca786a66640180e8fdd8c92e
thorgeir93 <thorgeirsigurd@gmail.com>
2018-01-17 12:34:24 +0000

    Change the brightness funciton. Add the i3lock. Update the status bar.

--- microprograms/bright_up.sh
+++ microprograms/bright_up.sh
@@ -1,7 +1,39 @@
-max_brightness=$(cat /sys/class/backlight/ideapad/max_brightness)
-brightness=$(cat /sys/class/backlight/ideapad/brightness)
+#!/bin/bash
+max_brightness=1.0
+current_brightness=`xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+scale=0.05
 
-if (("$brightness" < "$max_brightness")); then
-  let brightness=$brightness+1
-  echo "echo $brightness > /sys/class/backlight/ideapad/brightness" | sudo bash
-fi
+echo "Current brightness = [$current_brightness]"
+
+if [[ $1 == "UP" ]]; then
+    if [ `echo $current_brightness'<'$max_brightness | bc -l` == 1 ]; then
+        current_brightness=`echo $current_brightness+$scale | bc`
+        echo "Current brightness = [$current_brightness]"
+        xrandr --output eDP-0 --brightness $current_brightness
+    else
+        echo "Already in maximum brightness!"
+        echo "Brightness = [$current_brightness]"
+    fi
+
+elif [[ $1 == "DOWN" ]]; then
+    if [ `echo $current_brightness'>0.5' | bc -l` == 1 ]; then
+        current_brightness=`echo $current_brightness-$scale | bc`
+        echo "Current brightness = [$current_brightness]"
+        xrandr --output eDP-0 --brightness $current_brightness
+    else
+        echo "Brightness limit reached!"
+        echo "Brightness = ['$current_brightness']"
+    fi
+else
+    echo "Something is missing ..."
+    echo "Pass in either DOWN or UP argument."
+fi 
+
+
+#max_brightness=$(cat /sys/class/backlight/*/max_brightness)
+#brightness=$(cat /sys/class/backlight/*/brightness)
+
+#if (("$brightness" < "$max_brightness")); then
+  #let brightness=$brightness+65
+  #echo "echo $brightness > /sys/class/backlight/*/brightness" | sudo bash
+#fi
