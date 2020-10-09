b570a8972007ccf914e210236b7611f0664f26bf
thorgeir93 <thorgeirsigurd@gmail.com>
2020-10-09 11:43:07 +0000

    Replace py3.5->py3.4. Optimize set vim settings.

--- microprograms/vimrc.sh
+++ microprograms/vimrc.sh
@@ -10,7 +10,10 @@
 # Just to be sure the commands will not 
 # be execute alongside mines.
 sleep 0.3
-xdotool key colon s e t space t s equal 4 Return 
-xdotool key colon s e t space e x p a n d t a b Return 
-xdotool key colon r e t a b Return 
-xdotool key colon s e t space b g equal d a r k Return
+
+# et -> expandtab
+# ret -> retab
+# fdm -> foldmethod
+# se -> set
+xdotool type ":se ts=4 bg=dark fdm=indent et | ret" && \
+    xdotool key Return
